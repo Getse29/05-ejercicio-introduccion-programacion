df4baac8685566f822d4ac40b3cf848e2366a5cf
Josue Getsemani Sandoval Villarreal <getsemanilol29@gmail.com>
2022-12-11 19:39:11 -0800

    ejercicio bootcamp java

---
+++ src/com/getsemani/Main.java
@@ -0,0 +1,33 @@
+package com.getsemani;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // Tipos de datos
+    // Enteros
+    byte b = 127;
+    short s = 32767;
+    int i = 2147483647;
+    long l = 9223372036854775807L;
+    // Decimales
+    float f = 3.4028235E38F;
+    double d = 1.7976931348623157E308;
+    // Caracteres
+    char c = 'a';
+    // Booleanos
+    boolean bo = true;
+    // Cadenas
+    String str = "Hola mundo";
+    // Imprimir
+    System.out.println("byte: " + b);
+    System.out.println("short: " + s);
+    System.out.println("int: " + i);
+    System.out.println("long: " + l);
+    System.out.println("float: " + f);
+    System.out.println("double: " + d);
+    System.out.println("char: " + c);
+    System.out.println("boolean: " + bo);
+    System.out.println("String: " + str);
+  }
+
+}
